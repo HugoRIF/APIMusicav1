2a6ce65fb2b659de41180e454df5b7fa133f2488
HugoRIF <42448456+HugoRIF@users.noreply.github.com>
2019-01-13 22:57:46 -0600

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'usuario@db-rep',
 	'password' => 'Root1234',
 	'database' => 'bd_rep',
-	'dbdriver' => '',
+	'dbdriver' => 'mysql',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),

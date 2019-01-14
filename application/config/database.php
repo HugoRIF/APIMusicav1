17f1ffa49500a42a1d8da878dfe1b53ec6b58c77
HugoRIF <42448456+HugoRIF@users.noreply.github.com>
2019-01-13 22:49:08 -0600

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'cp:bd-rep.database.windows.net,1433',
+	'hostname' => 'bd-rep.database.windows.net',
 	'username' => 'usuario@db-rep',
 	'password' => 'Root1234',
 	'database' => 'bd_rep',
